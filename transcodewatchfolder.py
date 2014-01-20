c2efba451350bdb67a566efe2397c08b44568c11
hinfeyg2 <hinfeyg2@gmail.com>
2014-01-20 22:34:51 +0000

    Work on destination path
    Output path is now 1 directory up from folder being watched and named
    after the input file.
    The if removed option has been fixed.

--- transcodewatchfolder.py
+++ transcodewatchfolder.py
@@ -3,7 +3,8 @@ from subprocess import Popen, PIPE
 import re
 
 
-path_to_watch = "./TestWatchFolder"
+path_to_watch = "/Users/gavinhinfey/Desktop/TESTWATCHFOLDER/"
+
 before = dict ([(f, None) for f in os.listdir (path_to_watch)])
 while 1:
   time.sleep (3)
@@ -22,22 +23,21 @@ while 1:
 			time.sleep (5)
 	print "Added: ", ", ".join (added)
 	
-	
-	
-	
 	abspathname = os.path.abspath(currentpathandfile)
 	
-	removemov = abspathname[:-4]
-	removemov = str(removemov)
-	removemov.split('\\')
-
-	morted = removemov[-1]
-	print 
-	destpath = "C:\Users\assistant1\Desktop\TestWatchFolder\MXF" + '\\' + str(morted)
+	abspathnameLessEXT = abspathname[:-4]
+	abspathnameLessEXT = abspathnameLessEXT.split('/')
+	filenameLestEXT = abspathnameLessEXT[-1]
+	abspathnameLessFilenameEXT = abspathnameLessEXT.pop(-1)
+	uponedir = abspathnameLessEXT.pop(-1)
 	
-	dogs = "ffmpeg -i " + '"' + str(abspathname) + '"' + " -pix_fmt yuv422p -vcodec mpeg2video -non_linear_quant 1 -flags +ildct+ilme -top 1 -dc 10 -intra_vlc 1 -qmax 3 -lmin " + '"1*QP2LAMBDA"' + " -vtag xd5c -rc_max_vbv_use 1 -rc_min_vbv_use 1 -g 12 -b:v 50000k -minrate 50000k -maxrate 50000k -bufsize 8000k -acodec pcm_s16le -ar 48000 -bf 2 -ac 2 " + '"' + str(destpath) + '"' + ".mxf"
-	
-	Popen(dogs)
-	if removed: print "Removed: ", ", ".join (removed)
+	abspathWatchFolder = '/'.join(abspathnameLessEXT)
+	destpath = abspathWatchFolder + '/' + filenameLestEXT + '_xdcam_mxf' + '/' + filenameLestEXT
+	os.system("mkdir " + abspathWatchFolder + '/' + filenameLestEXT + "_xdcam_mxf")
+	"""
+	runRender = "ffmpeg -i " + '"' + str(abspathname) + '"' + " -pix_fmt yuv422p -vcodec mpeg2video -non_linear_quant 1 -flags +ildct+ilme -top 1 -dc 10 -intra_vlc 1 -qmax 3 -lmin " + '"1*QP2LAMBDA"' + " -vtag xd5c -rc_max_vbv_use 1 -rc_min_vbv_use 1 -g 12 -b:v 50000k -minrate 50000k -maxrate 50000k -bufsize 8000k -acodec pcm_s16le -ar 48000 -bf 2 -ac 2 " + '"' + str(destpath) + '"' + ".mxf"
+	Popen(runRender)
+	"""
+  if removed: print "Removed: ", ", ".join (removed)
 	
   before = after
